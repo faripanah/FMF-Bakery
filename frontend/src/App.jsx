70f483f37f9e1aa5f88ac3cbdfde73651b97c083
faripanah <farnaz.jamilpanah@metropolia.fi>
2025-03-12 11:44:18 +0200

    module.css files updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,62 +1,47 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
 
-// pages & components
+// Pages & Components
 import Navbar from "./components/Navbar/NavBar";
 import FindShop from "./pages/FindShop";
-//import EditJobPage from "./pages/EditJobPage";
-//import NotFoundPage from "./pages/NotFoundPage";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/signUp/SignUP";
 import AdminPage from "./pages/AdminPage";
+import Hero from "./components/Hero/Hero"
+import Footer from "./components/Footer/Footer"
+import Menu from "./components/Menu/Menu";
+
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     return user && user.token ? true : false;
   });
-  
 
   return (
     <div className="App">
       <BrowserRouter>
-      <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated}/>
+        <Navbar isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />
         <div className="content">
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/jobs/:id" element={<JobPage isAuthenticated={isAuthenticated} />} />
-            <Route
-              path="/jobs/add-job"
-              element={isAuthenticated ? <AddJobPage /> : <Navigate to="/signup" />}
-            />           
-            <Route
-              path="/edit-job/:id"
-              element={isAuthenticated ? <EditJobPage /> : <Navigate to="/signup" />}
-            />
-           <Route  
+            {/* ✅ Add a Route for Home */}
+            <Route path="/" element={<Hero />} />
+
+            <Route  
               path="/signup" 
               element={isAuthenticated ? <Navigate to="/" /> : <SignUp setIsAuthenticated={setIsAuthenticated} />} 
-
             />
             <Route
               path="/login"
-              element={
-                isAuthenticated ? (
-                  <Navigate to="/" />
-                ) : (
-                  <Login setIsAuthenticated={setIsAuthenticated} />
-                )
-              }
+              element={isAuthenticated ? <Navigate to="/" /> : <Login setIsAuthenticated={setIsAuthenticated} />}
             />
             <Route
               path="/secret-dashboard"
-              element={
-               (isAuthenticated && user.role === "admin")  ? 
-                  <AdminPage />: <Navigate to="/" />
-              }
+              element={(isAuthenticated && user.role === "admin") ? <AdminPage /> : <Navigate to="/" />}
             />
+             <Route path="/menu" element={<Menu />} />
             <Route path="/find-shop" element={<FindShop />} />
-            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/" element={<Footer />} />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,39 +1,35 @@
-import logo from '../assets/Images/bakerylogo.png';
+import logo from '../../assets/Images/bakerylogo.png';
 import { FaSearch } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
 import { FaUserLarge } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import styles from "./NavBar.module.css";
 
 const Navbar = () => {
     const user = false;
     const isAdmin = true;
     return(
-        <header className='navbar'>
-            <div className='logo'>
+        <header className={styles.navbar}>
+            <div className={styles.logo}>
                 <img src={logo} alt="Bakery Logo" />
             </div>
             <nav>
-                <ul className="nav-links">
+                <ul className={styles.navLinks}>
                     <li><Link to="/" >Home</Link></li>
                     <li><Link to="/menu" >Menu</Link></li>
                     <li><Link to="/contact" >Contact Us</Link></li>
                     <li><Link to="/find-shop" >Find Store</Link></li>
-                  
                 </ul>
                 {isAdmin && <Link to={"/secret-dashboard"}> Dashboard </Link>}
-               
             </nav>
-              {/* Icons Section */}
-              <div className="icons">
-                  
-                    <FaSearch />
-                    <FaCartShopping />
-                    <Link to="/signup"><FaUserLarge /></Link>
-                </div>
-
-
-
+            {/* Icons Section */}
+            <div className={styles.icons}>
+                <FaSearch />
+                <FaCartShopping />
+                <Link to="/signup"><FaUserLarge /></Link>
+            </div>
         </header>
-    )
-}
+    );
+};
+
 export default Navbar;
